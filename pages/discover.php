21689d972e12be6268aa51551bf4cd0b662b117a
alessio-scuola <alessio.gualtieri24@istitutotecnicomarconipilla.edu.it>
2026-03-22 20:51:52 +0100

    page discover modified

--- pages/discover.php
+++ pages/discover.php
@@ -6,14 +6,11 @@
     <title>Chi siamo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    <?php require_once(__DIR__ . '/../includes/header.php'); ?>
 
-    <div class="container flex-grow-1 d-flex justify-content-center align-items-center">
-        
-    </div>
+    <?php require_once(__DIR__ . '/../includes/header.php'); ?>
 
     <?php require_once(__DIR__ . '/../includes/footer.php'); ?>
     
